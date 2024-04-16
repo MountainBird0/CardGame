78c630fade3776572b47de0d80479119237300d7
RaeGyu <wogns9769@gmail.com>
2024-04-16 10:21:32 +0900

    카드섞기 중복 제거

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -7,11 +7,12 @@ public class Board : MonoBehaviour
 {
     public GameObject card;
     int[] arr;
+    int length;
     void Start()
     {
         arr = new int[] {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7};
         //arr= arr.OrderBy(x=>Random.Range(0f,7f)).ToArray();
-
+        length = arr.Length;
 
         RandomArray();
         for(int i=0 ;i <16; i++){
@@ -22,17 +23,30 @@ public class Board : MonoBehaviour
             go.transform.position = new Vector2(x,y);
             go.GetComponent<Card>().Setting(arr[i]);
         }
-        GameManager.Instance.carCount = arr.Length-2;
+        GameManager.Instance.carCount = length - 2;
     }
     
     private void RandomArray(){
         int num;
-        for (int i = 0; i < arr.Length; i++)
+        bool check;
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
         {
-            int random = Random.Range(0, arr.Length);
-            num = arr[random];
-            arr[random] = arr[i];
-            arr[i] = num;
+            check = true;
+            int random = Random.Range(0, length);
+            for(int j=0; j<i;j++){
+                if(array[j]==random){
+                    i--;
+                    Debug.Log("돌아가기");
+                    check = false;
+                }
+            }
+            if(check){
+                array[i] = random;
+                num = arr[random];
+                arr[random] = arr[i];
+                arr[i] = num;
+            }
         }
     }
     // Update is called once per frame
